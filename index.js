76218be382628edc3bd29d35dca2c322f7e2d505
Sergio Andrade <sergioandrade868@gmail.com>
2023-07-28 12:08:20 -0300

    agrega arrays

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
+
 class Alumno {
-    constructor() {
+    constructor(nombre, promedio) {
         this.nombre = nombre;
         this.promedio = promedio;
     }
+
 }
+
 function nota() {
     let nota1 = parseInt(prompt("Ingrese la 1er nota"));
     let nota2 = parseInt(prompt("Ingrese la 2er nota"));
@@ -13,33 +16,100 @@ function nota() {
     Alumno.promedio = promedio.toFixed(2);
     return Alumno.promedio;
 }
-function listaAlumnos(num) {
 
-    for (let i = 0; i < Alumnos.length; i++) {
-        if (Alumnos[i].promedio >= 7) {
-            console.log(Alumnos[i].nombre + " APROBADO " + " Nota final: " + Alumnos[i].promedio)
-        } else {
-            console.log(Alumnos[i].nombre + " DESAPROBADO " + " Nota final: " + Alumnos[i].promedio)
-        }
+function agregarAlumnos() {
+    let op = prompt("¿Desea añadir un alumno al principio de la lista o al final?");
 
+    if (op == "principio") {
+        let nom = prompt("Ingrese el nombre del alumno");
+        let pro = nota();
+        let alumno = new Alumno(nom, pro);
+        listaAlumnos.unshift(alumno);
+
+    } else if (op == "final") {
+        let nom = prompt("Ingrese el nombre del alumno");
+        let pro = nota();
+        let alumno = new Alumno(nom, pro);
+        listaAlumnos.push(alumno);
+
+    } else {
+        console.log("No ha seleccionado una opción correcta");
     }
 
 }
-let num = parseInt(prompt("Cuantos alumnos desea agregar"));
-let i = 0;
-const Alumnos = [num]
 
-while (i < num ) {
-    Alumno.nombre = prompt("ingrese nombre del Alumno");
-    nota();
+function eliminarAlumnos() {
 
-    console.log("Nota final de " + Alumno.nombre + " " + Alumno.promedio);
-    if (Alumno.promedio >= 7) {
-        console.log("APROBADO");
+    let eliminar = prompt("¿Qué alumno desea eliminar?");
+    listaAlumnos.splice(eliminar, 1);
+}
+
+function lista() {
+
+    for (let i = 0; i < listaAlumnos.length; i++) {
+        if (listaAlumnos[i].promedio >= 7) {
+            console.log(listaAlumnos[i].nombre + " APROBADO " + " Nota final: " + listaAlumnos[i].promedio);
+            console.log("====================");
+        } else {
+            console.log(listaAlumnos[i].nombre + " DESAPROBADO " + " Nota final: " + listaAlumnos[i].promedio);
+            console.log("====================");
+
+        }
+    }
+
+}
+
+function crearAlumnos(num) {
+
+    for (let i = 0; i < num; i++) {
+        let nombre = prompt("Ingrese el nombre del alumno");
+        let promedio = nota();
+        let alumno = new Alumno(nombre, promedio);
+        listaAlumnos.push(alumno);
+    }
+
+
+}
+const num = parseInt(prompt("Ingrese la cantidad de alumnos para agregar"));
+const listaAlumnos = [];
+crearAlumnos(num);
+
+console.log("MENÚ PRINCIPAL");
+console.log("1.Agregar alumnos");
+console.log("2.Eliminar alumno");
+console.log("3.Mostrar alumnos y sus notas");
+
+
+i = 0;
+let seguir = true;
+
+while (seguir) {
+
+    let opcion = prompt("Ingrese una opción del 1 al 3");
+
+    switch (opcion) {
+        case "1":
+            agregarAlumnos();
+            break;
+        case "2":
+            eliminarAlumnos();
+            break;
+        case "3":
+            lista();
+            break;
+        default:
+            console.log("OPCIÓN INVALIDA");
+            break;
+    }
+
+    seguir = prompt("Desea volver al menú principal");
+
+    if (seguir == "si") {
+        seguir = true;
     } else {
-        console.log("DESAPROBADO");
+        seguir = false;
     }
     i++;
 }
-/* console.log("LISTA DE AlumNOS")
-listaAlumnos(); */
+
+console.log("Nos vemos pronto!!");
